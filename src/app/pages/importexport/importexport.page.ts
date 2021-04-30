a96fe9e68faf477d1efc7c45efac6a15f2db258e
Claus J. Bauer <claus@clausjbauer.de>
2021-04-30 16:25:27 +0200

    Version 0.1.1 (Import/Export) 	modified:   config.xml 	modified:   package-lock.json 	modified:   src/app/pages/importexport/importexport.page.html 	modified:   src/app/pages/importexport/importexport.page.ts 	modified:   src/app/pages/ueber/ueber.page.html

--- src/app/pages/importexport/importexport.page.ts
+++ src/app/pages/importexport/importexport.page.ts
@@ -8,18 +8,25 @@ import { DbService } from 'src/app/services/db.service';
 
 const { Filesystem } = Plugins;
 
+interface myType {
+  Dateiname: string, 
+  Pfad: string, 
+  LetzteAenderung: string
+};
+
 @Component({
   selector: 'app-importexport',
   templateUrl: './importexport.page.html',
   styleUrls: ['./importexport.page.scss'],
 })
+
 export class ImportexportPage implements OnInit {
 
   constructor(private router: Router,
               private plt: Platform,
               private dbService: DbService,
               private alertCtrl: AlertController,
-              // private toastCtrl: ToastController,
+              private toastCtrl: ToastController,
               // private route: ActivatedRoute,
               private file: File,
               private androidPermissions: AndroidPermissions
@@ -28,7 +35,9 @@ export class ImportexportPage implements OnInit {
 
   intDBVersion: string = '1';
   lblMeldung: any;
-  directories: any = [];
+  sortierteDateiliste: any = [];
+  // sortierteDateiliste: Array<myType[]> = new Array();
+  // metaDaten: string = '';
   thePath: any;
   theFile: Entry;
   External_READ_Persmission: boolean;
@@ -63,6 +72,14 @@ export class ImportexportPage implements OnInit {
     await alert.present();
   }
 
+  async presentToast(theMessage: string) {
+    const toast = await this.toastCtrl.create({
+      message: theMessage,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   async checkReadWritePermissions() {
     this.plt.ready().then(() => {
       this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(
@@ -92,55 +109,31 @@ export class ImportexportPage implements OnInit {
       }
     });
   }
-//-------------------------------------------------------------------------------------
 
-  private verifyIfExists(ITEM, LIST) {
-    let verification = false;
-    console.log('LIST: ' + JSON.stringify(LIST));
-    console.log('ITEM: ' + JSON.stringify(ITEM));
-    for (let i = 0; i < LIST.length; i++) {
-      if (LIST[i] === ITEM[0]) {
-        verification = true;
-        break;
-      }
-    }
-    return verification;
-  }
-
-  async readdir() {
-    try {
-      let ret = await Filesystem.readdir({
-        path: '',
-        directory: FilesystemDirectory.External
-      });
-      this.directories = ret;
-      if (this.verifyIfExists(['Export_SysDiaPuls.db'], ret.files)) {
-        console.log("Export_SysDiaPuls.db gefunden!");
-      }
-      else {
-        // Do something else
-      }
-    } 
-    catch(e) {
-      console.log('Verzeichnis kann nicht gelesen werden: ' + e);
-    }
-  }
-
-//-------------------------------------------------------------------------------------
   async loadDocuments() {
     this.plt.ready().then(() => {
       this.lblMeldung = document.getElementById('txtMeldung').getElementsByTagName('textarea')[0];
       this.checkReadWritePermissions();
- 
+
       if ( this.file.externalApplicationStorageDirectory.length > 0 ) {
         console.log('this.file.externalApplicationStorageDirectory: ' + this.file.externalApplicationStorageDirectory);
         // this.readdir();
-        this.file.checkDir(this.file.externalApplicationStorageDirectory, 'files').then(_ => {
+        this.file.checkDir(this.file.externalApplicationStorageDirectory, 'files')
+        .then(_ => {
           // console.log('Verzeichnis SysDiaPuls existiert');
-          this.file.listDir(this.file.externalApplicationStorageDirectory, 'files').then(res => {
-            this.directories = res;
+          this.file.listDir(this.file.externalApplicationStorageDirectory, 'files')
+          .then(res => {
+            this.sortierteDateiliste = res.sort(function(a,b) {
+              if ( a.name < b.name ) { return 1; }
+              if ( a.name > b.name ) { return -1; }
+              return 0;
+            });
+            // this.dateiliste.forEach(element => {
+            //   this.ergaenzeDateiliste(element);
+            // });
           });
-        }).catch(err => {
+        })
+        .catch(err => {
           console.log('Verzeichnis SysDiaPuls existiert nicht');
         });
       } else {
@@ -148,7 +141,50 @@ export class ImportexportPage implements OnInit {
       }
     });
   }
+
+  /*
+  async ergaenzeDateiliste(element) {
+    let derName: string = element.name;
+    let derPfad: string = element.fullPath;
+
+    function success(metadata) {
+      try {
+        // this.metaDaten = (new Date(metadata.modificationTime)).toLocaleString();
+        // let theDate: string = (new Date(metadata.modificationTime)).toLocaleString();              // 'de-DE', { timeZone: 'GMT+0100' })
+        // if ( this.sortierteDateiliste === undefined ) {
+        //   this.sortierteDateiliste = [];
+        //   console.log('sortierteDateiliste initialisiert')
+        // }
+        let lm: string = (new Date(metadata.modificationTime)).toLocaleString();
+        this.sortierteDateiliste.push({Name: derName, Pfad: derPfad, LetzteAenderung: lm});
+        console.log('Name: ' + derName + ', Pfad: ' + derPfad + ', LetzteAenderung: ' + lm);
+      } catch (e) {
+        console.log("Fehler ergaenzeDateiliste: " + e);
+      }
+    }
+
+    function fail(error) {
+      console.log(JSON.stringify(error));
+    }
   
+    await element.getMetadata(success, fail);
+    try {
+      // let values_to_push: myType[] = [];
+      // values_to_push["Dateiname"] = element.name;
+      // values_to_push["Pfad"] = element.fullPath;
+      // values_to_push["LetzteAenderung"] = this.metaDaten;
+      // console.log('values_to_push: ' + JSON.stringify(values_to_push));
+      // this.sortierteDateiliste.push([Dateiname: element.name, Pfad: element.fullPath, LetzteAenderung: this.metaDaten]);
+      // this.sortierteDateiliste.push(values_to_push);
+      // let anzlen: number = this.sortierteDateiliste.length-1;
+      // console.log('Anz. Eintraege: ' + JSON.stringify(anzlen));
+      // console.log('neuer Eintrag: ' + JSON.stringify(this.sortierteDateiliste[anzlen]));
+    } catch(e) {
+      console.log('Fehler ergaenzeDateiliste: ' + e);
+    }
+  }
+  */
+
   ngOnInit() {
     this.loadDocuments();
   }
@@ -168,7 +204,7 @@ export class ImportexportPage implements OnInit {
       let sqldatabearbeitet: string = this.bearbeiteDaten(theData);
       await this.writeSqlFile(sqldatabearbeitet);
       await this.loadDocuments();
-      this.lblMeldung.value = "Die Datenbank wurde erfolgreich exportiert.";
+      this.presentToast("Die Datenbank wurde erfolgreich exportiert.");
     }
 
     console.log("Datenbankexport beendet");
@@ -247,16 +283,17 @@ export class ImportexportPage implements OnInit {
       })
     } else {
       console.log('dies ist keine geeignete SQL-Datei: ' + file.name);
+      this.presentToast('Dies ist keine geeignete Importdatei!');
     }
     if ( sqldata.length > 0 ) {
       // let sqldatabearbeitet: string = this.bearbeiteDaten(sqldata);
       // console.log("sqldatabearbeitet: " + sqldatabearbeitet);
       let ret: any = await this.theImportFromSql(sqldata);
       let ianz: number = parseInt(ret.toString()) - 2;
-      this.lblMeldung.value = 'Die bisherigen Einträge wurden durch ' + ianz + ' importierte Einträge ersetzt.';
+      this.presentToast('Die bisherigen Einträge wurden durch ' + ianz + ' importierte Einträge ersetzt.');
     } else {
       console.log('Die ausgewählte Date ist leer');
-      this.lblMeldung.value = 'Die ausgewählte Datei ist leer!';
+      this.presentToast('Die ausgewählte Datei ist leer!');
     }
 
     console.log("Datenbankimport beendet");
@@ -308,165 +345,18 @@ export class ImportexportPage implements OnInit {
       console.error('Datei ' + aktDateiname + ' kann nicht geschrieben werden: ', e);
     }
   }
-  
-// -----------------------------------------------------------------------
-/*
-  async fileWrite() {
-    try {
-      const result = await Filesystem.writeFile({
-        path: 'SysDiaPuls/SysDiaPuls.txt',
-        data: "Yippie: SysDiaPuls war erfolgreich!",
-        directory: FilesystemDirectory.ExternalStorage,
-        encoding: FilesystemEncoding.UTF8,
-        recursive: true
-      });
-      await this.fileRead();
-      console.log('ExternalStorage: ' + JSON.stringify(FilesystemDirectory.ExternalStorage));
-      console.log('Datei geschrieben', result);
-    } catch(e) {
-      console.error('Datei konnte nicht geschrieben werden', e);
-    }
+
+  zeigeDatumDerDatei(derName: string): string {
+    let dasJahr: string = derName.substr(0, 4);
+    let derMonat: string = derName.substr(4, 2);
+    let derTag: string = derName.substr(6, 2);
+    let dieStunde: string = derName.substr(9, 2);
+    let dieMinute: string = derName.substr(11, 2);
+    return derTag + '.' + derMonat + '.' + dasJahr + ' ' + dieStunde + ':' + dieMinute;
   }
 
-  async fileRead() {
-    let contents = await Filesystem.readFile({
-      path: 'SysDiaPuls/SysDiaPuls.txt',
-      directory: FilesystemDirectory.ExternalStorage,
-      encoding: FilesystemEncoding.UTF8
-    });
-    console.log('ExternalStorage: ' + JSON.stringify(FilesystemDirectory.ExternalStorage));
-    console.log(JSON.stringify(contents));
-    if ( this.lblMeldung == null ) {
-      console.log('this.lblMeldung.value is null');
-    } else {
-      this.lblMeldung.value = JSON.stringify(contents);
-    }
+  zeigeNameDerDatei(derName: string): string {
+    let pos: number = derName.lastIndexOf('_') + 1;
+    return derName.substr(pos);
   }
-
-  async fileAppend() {
-    try {
-      let ret = await Filesystem.appendFile({
-        path: 'SysDiaPuls/SysDiaPuls.txt',
-        data: "\nWEITERE TESTS",
-        directory: FilesystemDirectory.ExternalStorage,
-        encoding: FilesystemEncoding.UTF8
-      });
-      console.log('fileAppend(): ' + JSON.stringify(ret));
-      this.fileRead();
-    } catch(e) {
-      console.error('Datei konnte nicht erweitert werden: ', e);
-    }
-  }
-
-  async fileDelete() {
-    await Filesystem.deleteFile({
-      path: 'SysDiaPuls/SysDiaPuls.txt',
-      directory: FilesystemDirectory.ExternalStorage
-    });
-  }
-
-  async mkdir() {
-    try {
-      let ret = await Filesystem.mkdir({
-        path: 'SysDiaPuls',
-        directory: FilesystemDirectory.ExternalStorage,
-        recursive: false // like mkdir -p
-      });
-      if ( this.lblOrdner == null ) {
-        console.log('this.lblOrdner.value is null');
-      } else {
-        this.lblOrdner.value = JSON.stringify(ret);
-      }
-      console.log('mkdir(): ' + JSON.stringify(ret));
-      console.log('Verzeichnis SysDisPuls angelegt');
-    } catch(e) {
-      console.error('Verzeichnis konnte nicht angelegt werden: ', e);
-    }
-  }
-
-  async rmdir() {
-    try {
-      let ret = await Filesystem.rmdir({
-        path: 'SysDiaPuls',
-        directory: FilesystemDirectory.ExternalStorage,
-        recursive: false,
-      });
-      if ( this.lblOrdner == null ) {
-        console.log('this.lblOrdner.value is null');
-      } else {
-        this.lblOrdner.value = JSON.stringify(ret);
-      }
-      console.log('rmdir(): ' + JSON.stringify(ret));
-    } catch(e) {
-      console.error('Verzeichnis SysDiaPuls konnte nicht entfernt werden', e);
-    }
-  }
-
-  async readdir() {
-    try {
-      let ret = await Filesystem.readdir({
-        path: 'SysDiaPuls',
-        directory: FilesystemDirectory.ExternalStorage
-      });
-      console.log('readdir(): ' + JSON.stringify(ret));
-    } catch(e) {
-      console.error('Verzeichnis SysDiaPuls konnte nicht gelesen werden', e);
-    }
-  }
-
-  async stat() {
-    try {
-      let ret = await Filesystem.stat({
-        path: 'SysDiaPuls/SysDiaPuls.txt',
-        directory: FilesystemDirectory.ExternalStorage
-      });
-      console.log('stat(): ' + JSON.stringify(ret));
-    } catch(e) {
-      console.error('kein Status der Datei text.txt', e);
-    }
-  }
-
-  async readFilePath() {
-    // Here's an example of reading a file with a full file path. Use this to
-    // read binary data (base64 encoded) from plugins that return File URIs, such as
-    // the Camera.
-    try {
-      let data = await Filesystem.readFile({
-        path: 'file:///var/mobile/Containers/Data/Application/22A433FD-D82D-4989-8BE6-9FC49DEA20BB/Documents/text.txt'
-      });
-    } catch(e) {
-      console.error('Datei kann nicht gelesen werden', e);
-    }
-  }
-
-  async rename() {
-    try {
-      // This example moves the file within the same 'directory'
-      let ret = await Filesystem.rename({
-        from: 'SysDiaPuls/SysDiaPuls.txt',
-        to: 'SysDiaPuls/SysDiaPuls2.txt',
-        directory: FilesystemDirectory.ExternalStorage
-      });
-      console.log('Datei umbenannt');
-    } catch(e) {
-      console.error('Datei kann nicht umbenannt werden', e);
-    }
-  }
-
-  async copy() {
-    try {
-      // This example copies a file within the documents directory
-      let ret = await Filesystem.copy({
-        from: 'SysDiaPuls/SysDiaPuls.txt',
-        to: 'SysDiaPuls/SysDiaPuls2.txt',
-        directory: FilesystemDirectory.ExternalStorage
-      });
-    } catch(e) {
-      console.error('Datei kann nicht kopiert werden', e);
-    }
-  }
-*/
-// -----------------------------------------------------------------------
-
-
 }
